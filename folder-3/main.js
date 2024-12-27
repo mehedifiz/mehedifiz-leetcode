9d5452ee9f54185cd7c8457e49d957e24e4cbf5e
mehedifiz <mehedi.shanto.contract@gmail.com>
2024-12-27 12:51:13 +0600

    reduce

--- folder-3/main.js
+++ folder-3/main.js
@@ -2,13 +2,11 @@
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
-// var argumentsLength = function(...args) {
+var argumentsLength = function (...args) {
+  return args.length;
+};
 
-//    return (args.length)
-
-//  };
-
-//    console.log(argumentsLength(1, 2,45, 3 , null , false , {} , [] , "df")); // 3
+console.log(argumentsLength(1, 2, 45, 3, null, false, {}, [], "df"));
 
 /**
  * @param {number[]} arr
@@ -19,14 +17,36 @@ var map = function (arr, plusone) {
   let array = [];
 
   for (i = 0; i < arr.length; i++) {
-    array.push(plusone(arr[i] , i))
+    array.push(plusone(arr[i], i));
   }
-  return array
+  return array;
 };
 
 function plusone(num) {
-   return num + 1
-
+  return num + 1;
 }
 
-console.log(map([5 , 7], plusone));
+console.log(map([5, 7], plusone));
+
+// reduce
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function (nums, fn, init) {
+  let total = init;
+
+  for (let i = 0; i < nums.length; i++) {
+    total = fn(total, nums[i]);
+  }
+  return total;
+};
+
+function sum(accum, curr) {
+  return accum + curr;
+}
+
+console.log(reduce([1,4,4] ,sum, 0))
